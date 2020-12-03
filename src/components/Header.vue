3a5bc31a093bb72e4204e28afc6770c21c6f39e4
keigo9 <keigo.matsunari@gmail.com>
2020-12-03 18:07:43 +0900

    Headerの描写

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,17 @@
 <template>
-    <h1>Heaader</h1>
+    <h1>TotalPopulation of Prefecture and Chart</h1>
 </template>
 
-<script>
-export default {
-}
-</script>
-
 <style scoped>
+    h1{
+        background-color:green;
+        text-align:center;
+        margin-top:0px;
+    }
 
+    @media screen and (max-width: 480px) {
+        h1 {
+            font-size: 18px;
+        }
+    }
 </style>
