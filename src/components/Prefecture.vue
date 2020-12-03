5ffdd02f2ca78ccc629582bfcaaebb4a14d88848
keigo9 <keigo.matsunari@gmail.com>
2020-12-03 18:29:44 +0900

    APIから県庁所在地データ取得

--- src/components/Prefecture.vue
+++ src/components/Prefecture.vue
@@ -8,15 +8,31 @@
     <label for="mike">Mike</label>
     <br />
     <span>Checked names: {{ checkedNames }}</span>
+    <p @click="fetchApi">test</p>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import api from "./Apikey.js";
+
 export default {
   data() {
     return {
       checkedNames: [],
     };
+  },
+  methods: {
+    // APIから県庁所在地データ取得 
+    fetchApi() {
+      const response = axios.get(
+        `https://opendata.resas-portal.go.jp/api/v1/prefectures`,
+        {
+          headers: { "X-API-KEY": api.key },
+        }
+      );
+      return console.log(response);
+    },
   },
 };
 </script>
