fd596daa707568fc8358643e10d8ce0fabeadb36
keigo9 <keigo.matsunari@gmail.com>
2020-12-03 18:01:29 +0900

    コンポーネントファイルの生成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Prefecture />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Prefecture from './components/Prefecture.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Prefecture
   }
 }
 </script>
@@ -23,6 +25,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
